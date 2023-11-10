8f0c0d33c5a3f5853e5c9bdb1104d899a5b7da86
Zhuowei Zhang <zhuoweiz10@gmail.com>
2023-11-10 15:54:36 -0500

    imported Gregory Maldonado's code

---
+++ include/unique_thread.h
@@ -0,0 +1,30 @@
+#ifndef C__11TESTING__UNIQUE_THREAD_H
+#define C__11TESTING__UNIQUE_THREAD_H
+
+#include <memory>
+#include <mutex>
+#include <thread>
+
+#include "vex.h"
+
+namespace orange {
+typedef void (*func_ptr)(void);
+
+class unique_thread {
+  public:
+   unique_thread();
+
+   static void execute(func_ptr callback);
+
+   static std::unique_ptr<vex::mutex> &&get_lock();
+
+   static func_ptr get_callback();
+   static void set_callback(func_ptr callback);
+
+  private:
+   static std::unique_ptr<vex::mutex> lock;
+   static func_ptr _callback;
+};
+
+}  // namespace orange
+#endif
